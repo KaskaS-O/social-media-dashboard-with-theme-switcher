177f14f02f5e8e6768a0befaa4323ed926c5a8ca
KaskaS-O <kaska.salaban@gmail.com>
2021-11-09 15:00:35 +0100

    Fix switcher label for dark mode

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const slider = document.querySelector('.slider');
 const wrapper = document.querySelector('.wrapper');
+const sliderLabel = document.querySelector('.header__txt--switch');
 
 const changeTheme = () => {
     wrapper.classList.toggle('light');
     wrapper.classList.toggle('dark');
+    
+    if (wrapper.classList.contains('light')) {
+        sliderLabel.innerText = 'Dark mode';
+    } else if (wrapper.classList.contains('dark')) {
+        sliderLabel.innerText = 'Light mode';
+    }
 }
 
 slider.addEventListener('click', changeTheme);
